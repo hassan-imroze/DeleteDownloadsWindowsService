e6527e8f5fee07699f28783d73a8ff6695d121cd
Syed Mohammad Hassan <hassan.imroze@hotmail.com>
2020-10-31 21:35:15 +0600

    Completed the Service

---
+++ DeleteDownloadedFilesService/DeleteOlderFiles.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DeleteDownloadedFilesService
+{
+    public class DeleteOlderFiles
+    {
+        private readonly Timer _timer;
+        private string _deleteFolderPath;
+        private string _archiveFolderPath;
+        public DeleteOlderFiles(string deleteFolderPath,string archiveFolderPath)
+        {
+            _deleteFolderPath = deleteFolderPath;
+            _archiveFolderPath = archiveFolderPath;
+            var eightHours = (1000 * 60) * 60 * 8;
+            _timer = new Timer(eightHours) { AutoReset = true };
+            _timer.Elapsed += DeleteOldFiles;
+        }
+        public void Start()
+        {
+            _timer.Start();
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+        }
+        public void DeleteOldFiles(object sender, ElapsedEventArgs e)
+        {
+            var files = Directory.GetFiles(_deleteFolderPath);
+
+            foreach (var file in files)
+            {
+                var modifiedTimeUtc = File.GetLastWriteTimeUtc(file);
+                if((DateTime.UtcNow - modifiedTimeUtc).TotalDays >= 15)
+                {
+                    File.Copy(file, GetNewFilePath(file, modifiedTimeUtc)); 
+                    File.Delete(file);
+                }
+            }
+
+        }
+
+        private string GetNewFilePath(string file, DateTime modifiedDate)
+        {
+            string fileName = file.Split('\\').Reverse().FirstOrDefault();
+            string newFolderPath = $"{_archiveFolderPath}\\{modifiedDate:yyyy-MMM}";
+            if(!Directory.Exists(newFolderPath))
+            {
+                Directory.CreateDirectory(newFolderPath);
+            }
+            return $"{newFolderPath}\\{fileName}";
+        }
+    }
+}
--- DeleteDownloadedFilesService/Program.cs
+++ DeleteDownloadedFilesService/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Syroot.Windows.IO;
+using System;
+using Topshelf;
 
 namespace DeleteDownloadedFilesService
 {
@@ -10,6 +8,27 @@ namespace DeleteDownloadedFilesService
     {
         static void Main(string[] args)
         {
+            string downloadsPath = new KnownFolder(KnownFolderType.Downloads).Path;
+            string archiveFolderPath = "D:\\Downloads Archive";
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<DeleteOlderFiles>(s =>
+                {
+                    s.ConstructUsing(deleteOlderFiles => new DeleteOlderFiles(downloadsPath,archiveFolderPath));
+                    s.WhenStarted(deleteOlderFiles => deleteOlderFiles.Start());
+                    s.WhenStopped(deleteOlderFiles => deleteOlderFiles.Stop());
+                });
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("DeleteOlderDownloadFiles");
+                x.SetDisplayName("Delete Download Files");
+                x.SetDescription("Delete downloaded files older than 15 days from downloads folder and archive them");
+
+            });
+
+
+            Environment.ExitCode = (int)exitCode;
 
         }
     }
